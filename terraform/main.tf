937e5be089a5fc57d3bd43c60cec06972e87d69f
Singh Chauhan <vishal.singh-chauhan@capgemini.com>
2025-08-05 23:14:46 +0530

    more Functions

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,18 +3,19 @@ provider "aws" {
 }
 
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
-    
-    required_version = ">= 1.0"
+  }
+
+  required_version = ">= 1.0"
 }
 
+# Intentional Error 1: Invalid bucket name (must be globally unique and follow naming rules)
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "bucket"
+  bucket = "bucket"  # This will fail because "bucket" is too generic and likely already exists
 
   tags = {
     Name        = "BucketVishal"
@@ -22,3 +23,29 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
+# Intentional Error 2: Referencing a non-existent variable
+resource "aws_instance" "test" {
+  ami           = var.non_existent_ami_id  # This variable is not defined
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TestInstance"
+  }
+}
+
+# Intentional Error 3: Typo in block name (should be lifecycle, not life_cycle)
+resource "aws_security_group" "sg_error" {
+  name        = "bad-sg"
+  description = "SG with intentional typo"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  life_cycle {  # Incorrect spelling
+    prevent_destroy = true
+  }
+}
